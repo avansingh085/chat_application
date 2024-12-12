0eadbde367e05f3aefc9757db2153010cfaf2175
Avan Singh <avansingh085@gmail.com>
2024-12-12 23:31:49 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import {setSocket} from './globalSlice.js';
 import { useEffect } from 'react';
 import {io} from 'socket.io-client'
-const socket=io('https://chat-backend-9s3n.onrender.com');
+const socket=io('https://chat-application-0p9h.onrender.com');
 function App() {
   return (
     <BrowserRouter>
