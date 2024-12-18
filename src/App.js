132dc89a32541b3307c4ebbc9622c9908ae40f53
Avan Singh <avansingh085@gmail.com>
2024-12-18 10:55:55 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import {setSocket} from './globalSlice.js';
 import { useEffect } from 'react';
 import {io} from 'socket.io-client'
-const socket = io("ws://chat-backend-9s3n.onrender.com", {
+const socket = io("wss://chat-backend-9s3n.onrender.com", {
   transports: ['websocket'], 
   reconnection: true,
   debug: true,
